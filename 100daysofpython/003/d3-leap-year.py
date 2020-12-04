b72fafa6d5bb73175f64d8ffdfba8c61c4fcbfe8
Iñaki Narciso <ionicsoft@gmail.com>
2020-12-04 23:56:54 +0800

    ❸ Day 3: Leap year exercise

---
+++ 100daysofpython/003/d3-leap-year.py
@@ -0,0 +1,12 @@
+year = int(input('Which year do you want to check? '))
+
+is_divisible_by_4 = year % 4 == 0
+not_divisible_by_100 = year % 100 != 0
+is_divisible_by_400 = year % 400 == 0
+
+is_leap_year = is_divisible_by_4 and (not_divisible_by_100 or is_divisible_by_400)
+
+if is_leap_year:
+    print('Leap year')
+else:
+    print('Not leap year')
