0f94bbd69697ca58af69fdf8be1a5b36bdb948f2
Iñaki Narciso <ionicsoft@gmail.com>
2020-12-13 21:01:56 +0800

    ❽ Day 8 of 100 days of Python

---
+++ 100daysofpython/008/d8-blind-auction.py
@@ -0,0 +1,33 @@
+from os import name
+from os import system
+from d8_blind_auction_art import logo
+
+def clear():
+    system('cls' if name == 'nt' else 'clear')
+
+participants = {}
+
+clear()
+print(logo)
+print('Welcome to the secret auction program.')
+participant = input('What is your name? ')
+bid = int(input('What\'s your bid? $'))
+participants[participant] = bid
+has_more_bidders = input('Are there any other bidders? Type \'yes\' or \'no\'.\n') == 'yes'
+
+while has_more_bidders:
+    clear()
+    participant = input('What is your name? ')
+    bid = int(input('What\'s your bid? $'))
+    participants[participant] = bid
+    has_more_bidders = input('Are there any other bidders? Type \'yes\' or \'no\'.\n') == 'yes'
+
+clear()
+winner = None
+top_bid = 0
+for participant in participants:
+    if participants[participant] > top_bid:
+        winner = participant
+        top_bid = participants[participant]
+
+print(f'The winner of the bid is {winner} with a bid of ${top_bid}.')
