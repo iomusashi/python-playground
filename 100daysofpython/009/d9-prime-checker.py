f9bc1c155ec48b02aa5512bc95c1cc0b8cf1ba54
Iñaki Narciso <ionicsoft@gmail.com>
2020-12-13 21:42:09 +0800

    ❾ Day 9: Prime Checker

---
+++ 100daysofpython/009/d9-prime-checker.py
@@ -0,0 +1,11 @@
+def is_prime(number):
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+number = int(input('Enter a number: '))
+if is_prime(number):
+    print('It\'s a prime number.')
+else:
+    print('It\'s not a prime number.')
