29b7e25cbc351cc5beaa65a69b9ed840fe52bdf8
Iñaki Narciso <ionicsoft@gmail.com>
2020-12-15 23:56:43 +0800

    ❾ Day 9: Caeser Cipher Decoding functionality

--- 100daysofpython/009/d9-ceaser-cipher.py
+++ 100daysofpython/009/d9-ceaser-cipher.py
@@ -1,7 +1,7 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input('Type \'encode\' to encrypt, type \'decode\' to decrypt:\n')
-if direction != 'encode':
+if direction != 'encode' and direction != 'decode':
   raise SystemExit(0)
 
 text = input(f'Type your message to {direction}:\n').lower()
@@ -17,5 +17,18 @@ def encrypt(text, shift):
     cipher_text += letter
   print(f'The encoded text is {cipher_text}')
 
+def decrypt(cipher_text, shift):
+  text = ''
+  for char in cipher_text:
+    index = alphabet.index(char)
+    shifted = index - shift
+    letter = alphabet[shifted]
+    text += letter
+  print(f'The decoded cipher text is {text}')
+
 if direction == 'encode':
   encrypt(text, shift)
+elif direction == 'decode':
+  decrypt(text, shift)
+else:
+  raise SystemExit(0)
