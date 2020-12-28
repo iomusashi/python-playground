4dd7c101ccbe870998ac765e3eacce4d629bc95a
Iñaki Narciso <ionicsoft@gmail.com>
2020-12-28 21:21:31 +0800

    Autopep8 Day 2 codes

--- 100daysofpython/002/d2-tip-calculator.py
+++ 100daysofpython/002/d2-tip-calculator.py
@@ -1,6 +1,7 @@
 print('Welcome to the tip calculator.')
 total_bill = float(input('What was the total bill? $'))
-tip_percent = float(input('What percentage tip would you like to give? 10, 12, or 15? '))
+tip_percent = float(
+    input('What percentage tip would you like to give? 10, 12, or 15? '))
 num_people = int(input('How many people to split the bill? '))
 
 individual_bill = round(total_bill * (1 + (tip_percent / 100)) / num_people, 2)
