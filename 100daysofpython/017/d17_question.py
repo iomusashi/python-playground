758856ec475ab0e711b2c94e1bb1e28a72dda08a
Iñaki Narciso <ionicsoft@gmail.com>
2021-01-03 01:42:50 +0800

    Day 17: Question model

---
+++ 100daysofpython/017/d17_question.py
@@ -0,0 +1,10 @@
+class Question:
+
+    def __init__(self, text: str, answer: str):
+        """
+        Creates an instance of the Question class
+        text - The question text of type str
+        answer - The answer text of type str
+        """
+        self.text = text
+        self.answer = answer
