3d7523a88a31089ab23625c51cf9356d33fc6f5e
Iñaki Narciso <ionicsoft@gmail.com>
2021-01-03 01:58:32 +0800

    Day 17: Sneaky quiz manager

---
+++ 100daysofpython/017/d17_quiz_brain.py
@@ -0,0 +1,19 @@
+from d17_question import Question
+
+
+class QuestionBrain:
+    """
+    Organizes and randomizes quiz questions per attempt
+    """
+
+    def __init__(self, question_number: int, questions_list: [Question]):
+        """
+        Creates an instance of QuestionBrain
+        question_number - int
+        questions_list - [Question]
+        """
+        self.question_number = question_number
+        self.questions_list = questions_list
+
+    def next_question():
+        pass
