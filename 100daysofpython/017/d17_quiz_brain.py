3656bed3a78bfc0f55ef7d1689a759ebd2df017c
Iñaki Narciso <ionicsoft@gmail.com>
2021-01-04 22:36:31 +0800

    Day 17 of 100 days of Python

--- 100daysofpython/017/d17_quiz_brain.py
+++ 100daysofpython/017/d17_quiz_brain.py
@@ -6,14 +6,30 @@ class QuestionBrain:
     Organizes and randomizes quiz questions per attempt
     """
 
-    def __init__(self, question_number: int, questions_list: [Question]):
+    def __init__(self, questions_list: [Question]):
         """
         Creates an instance of QuestionBrain
         question_number - int
         questions_list - [Question]
         """
-        self.question_number = question_number
+        self.score = 0
+        self.question_number = 0
         self.questions_list = questions_list
 
-    def next_question():
-        pass
+    def has_next_questions(self) -> bool:
+        return self.question_number < len(self.questions_list)
+
+    def next_question(self):
+        current_question = self.questions_list[self.question_number]
+        self.question_number += 1
+        answer = input(
+            f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        if self.check_answer(answer, current_question.answer):
+            self.score += 1
+
+    def check_answer(self, answer: str, correct_answer: str) -> bool:
+        return answer.lower() == correct_answer.lower()
+
+    def print_score(self):
+        print(
+            f"Your final score is: {self.score} of {len(self.questions_list)}")
--- 100daysofpython/017/d17_quiz_game.py
+++ 100daysofpython/017/d17_quiz_game.py
@@ -1,6 +1,13 @@
 from d17_question import Question
+from d17_quiz_brain import QuestionBrain
 from d17_quiz_game_data import question_data
-
 
 question_bank = list(map(lambda dict: Question(dict["text"], dict["answer"]),
                          question_data))
+
+quiz = QuestionBrain(question_bank)
+
+while quiz.has_next_questions():
+    quiz.next_question()
+
+quiz.print_score()
