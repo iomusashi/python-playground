527b3558dbdc8b1d5d8f29110f93d3a4f82ef460
Iñaki Narciso <ionicsoft@gmail.com>
2021-01-12 23:15:21 +0800

    Day 18: Hirst spot painting

---
+++ 100daysofpython/018/d18-hirst-spot-painting.py
@@ -0,0 +1,40 @@
+from colorgram import extract
+from random import choice
+from random import randrange
+from turtle import Screen
+from turtle import Turtle
+
+DOT_SIZE = 20
+DISTANCE = 50
+GREYSCALE_COLOR_RANGE = 256
+rgb_colors = []
+turtle = Turtle()
+screen = Screen()
+
+
+def extract_colors() -> list:
+    image = "d18_hirst.jpg"
+    colors = extract(image, 30)
+    return list(map(lambda color: (color.rgb.r / GREYSCALE_COLOR_RANGE,
+                                   color.rgb.g / GREYSCALE_COLOR_RANGE,
+                                   color.rgb.b / GREYSCALE_COLOR_RANGE), colors))
+
+
+def hirst_spots(grid_size=10):
+    global DOT_SIZE
+    global DISTANCE
+    global rgb_colors
+    rgb_colors = extract_colors()
+    turtle.penup()
+    turtle.speed("fastest")
+    for i in range(grid_size):
+        for j in range(grid_size):
+            turtle.setposition(j * DISTANCE, i * DISTANCE)
+            turtle.pencolor(choice(rgb_colors))
+            turtle.pendown()
+            turtle.dot(DOT_SIZE)
+            turtle.penup()
+
+
+hirst_spots()
+screen.exitonclick()
