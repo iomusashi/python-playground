ae8b2556edc35cbe4365cf53c5f8fb76c9ef9072
Iñaki Narciso <ionicsoft@gmail.com>
2021-01-14 21:16:09 +0800

    D19: Add clear/reset functionality to edge a sketch

--- 100daysofpython/019/d19-etch-a-sketch.py
+++ 100daysofpython/019/d19-etch-a-sketch.py
@@ -5,19 +5,30 @@ screen = Screen()
 
 
 def move_forward(blocks=10):
+    global turtle
     turtle.forward(blocks)
 
 
 def move_backward(blocks=10):
+    global turtle
     turtle.backward(blocks)
 
 
 def move_left(angle=10):
+    global turtle
     turtle.left(angle)
 
 
 def move_right(angle=10):
+    global turtle
     turtle.right(angle)
+
+def clear():
+    global turtle
+    turtle.clear()
+    turtle.penup()
+    turtle.home()
+    turtle.pendown()
 
 
 screen.listen()
@@ -25,4 +36,5 @@ screen.onkey(key="w", fun=move_forward)
 screen.onkey(key="s", fun=move_backward)
 screen.onkey(key="a", fun=move_left)
 screen.onkey(key="d", fun=move_right)
+screen.onkey(key="c", fun=clear)
 screen.exitonclick()
