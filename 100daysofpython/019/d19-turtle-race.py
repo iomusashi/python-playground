a43e4a904cb6d21b56d920788f06f7cec3971422
Iñaki Narciso <inaki.narciso@appetiser.com.au>
2021-01-16 09:01:38 +0800

    Day 19: Place turtles on screen leading

--- 100daysofpython/019/d19-turtle-race.py
+++ 100daysofpython/019/d19-turtle-race.py
@@ -1,23 +1,24 @@
 from turtle import Screen, Turtle
 
+y_pos = [-100, -60, -20, 20, 60, 100]
+colors = ["red", "orange", "yellow", "green", "blue", "violet"]
+turtles = []
 screen = Screen()
-screen.setup(width=500, height=400)
-screen.exitonclick()
-turtles = []
-colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-y_axes = [-88, -50, -16, 16, 50, 88]
 
-user_bet = screen.textinput(
-    title="Make your bet",
-    prompt="Which turtle will win the race? Enter a color: ")
-turtles = []
-for i in range(0, 6):
-    tutle = Turtle(shape="turtle")
-    turtle.color(colors[i])
-    turtle.penup()
-    turtle.goto(x=-250, y=y_axes[i])
-    turtles.append(turtle)
-if user_bet in colors:
-    print("betcolor")
-else:
-    print("Invalid color to bet")
+def setup_screen_dimensions(width: int, height: int):
+    global screen
+    screen.setup(width=width, height=height)
+
+def setup_turtles():
+    global turtles
+    turtles = []
+    for i in range(0, 6):
+        turtle = Turtle(shape="turtle")
+        turtle.color(colors[i])
+        turtle.penup()
+        turtle.goto(-250, y_pos[i])
+        turtles.append(turtle)
+
+setup_screen_dimensions(width=500, height=400)
+setup_turtles()
+screen.exitonclick()
